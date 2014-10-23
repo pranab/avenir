5ed614992546bb77a9882c3bfe8c1880a10aaab3
Pranab Ghosh <pkghosh99@gmail.com>
2014-10-22 21:39:11 -0700

    initial spark commit

---
+++ spark/project/plugins.sbt
@@ -0,0 +1,15 @@
+logLevel := Level.Warn
+
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.11.2")
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.5.0")
+
+resolvers ++= Seq(
+  "Cloudera Repository" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
+  "Akka Repository" at "http://repo.akka.io/releases/",
+  "scala-tools" at "https://oss.sonatype.org/content/groups/scala-tools"
+)
+
+
---
+++ spark/src/main/scala/org/avenir/sanity/WordCount.scala
@@ -0,0 +1,29 @@
+package org.avenir.sanity
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+object WordCount {
+    def main(args: Array[String]) {
+      val master = args.length match {
+        case x: Int if x > 0 => args(0)
+        case _ => "local"
+      }
+      val sc = new SparkContext(master, "WordCount", System.getenv("SPARK_HOME"))
+      val input = args.length match {
+        case x: Int if x > 1 => sc.textFile(args(1))
+        case _ => sc.parallelize(List("pandas", "i like pandas"))
+      }
+      val words = input.flatMap(line => line.split(" "))
+      args.length match {
+        case x: Int if x > 2 => {
+          val counts = words.map(word => (word, 1)).reduceByKey{case (x,y) => x + y}
+          counts.saveAsTextFile(args(2))
+        }
+        case _ => {
+          val wc = words.countByValue()
+          println(wc.mkString(","))
+        }
+      }
+    }
+}
