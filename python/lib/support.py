1937bf480da8626b63b0d15cbb54a5afc2ef3527
Pranab Ghosh <pkghosh99@gmail.com>
2016-04-18 09:32:56 -0700

    hopkins stats for clusters

--- python/lib/support.py
+++ python/lib/support.py
@@ -5,6 +5,7 @@ import os
 import sys
 import numpy as np
 import jprops
+from random import randint
 
 # load configuration
 def getConfigs(configFile):
@@ -25,3 +26,29 @@ def extract_table_from_file(configs, file_param, col_indices_param):
 	data = np.loadtxt(data_file, delimiter=',')
 	tabl = data[:,col_indices]
 	return tabl
+	
+# finds minimum distance between each row f X1(m x p) and X2(n x p)
+# return (1 x m) array of min distances
+def find_min_distances(X1, X2):
+	min_dist = np.zeros(len(X1))
+	
+	for i,x1 in enumerate(X1):
+		dists = np.sqrt(np.sum((X2 - x1)**2,axis=1))
+		min_dist[i] = dists.min()
+
+	return min_dist
+
+# splits data randomly to create two arrays	
+def split_data_random(X, split_size):
+	#copy data
+	XC = np.copy(X)
+	
+	# split
+	lo = randint(1, len(X) - split_size) - 1
+	up = lo + split_size	
+	XSP = XC[lo:up:1]
+	
+
+	#remaining
+	XRE = np.delete(XC, np.s_[lo:up:1], 0)
+	return (XSP, XRE)
--- python/unsupv/cluster.py
+++ python/unsupv/cluster.py
@@ -21,7 +21,9 @@ if len(sys.argv) < 2:
 	print "usage: ./cluster.py <config_properties_file>"
 	sys.exit()
 
+# kmeans cluster
 def train_kmeans():
+	print "starting kmeans clustering..."
 	model = KMeans(n_clusters=num_clusters, init=init_strategy, n_init=num_inits, 
 	max_iter=num_iters, precompute_distances=precom_dist)
 	model.fit(X)
@@ -37,11 +39,22 @@ def train_kmeans():
 	cohesion = model.inertia_ / len(X)
 	print "cohesion:  %.3f" %(cohesion) 
 
+# loads file and extracts specific columns
+def extract_data(file_name_param, field_indices_param):
+	X = extract_table_from_file(configs, file_name_param, field_indices_param)	
+	#preprocess features
+	if (preprocess == "scale"):
+		X = sk.preprocessing.scale(X)
+	return X
+	
+# main
 configs = getConfigs(sys.argv[1])
 mode = configs["common.mode"]
+
+preprocess = configs["common.preprocessing"]
+
 if mode == "train":
 	#train
-	preprocess = configs["common.preprocessing"]
 	algo = configs["train.algo"]
 	num_clusters = int(configs["train.num.clusters"])
 	num_iters = int(configs["train.num.iters"])
@@ -72,4 +85,40 @@ if mode == "train":
 	
 	if algo == "kmeans":
 		train_kmeans()
-		
+	else:
+		print "invalid cluster algorithm"
+		sys.exit()
+			
+elif mode == "explore":
+	#calculate hopkins stats
+	print "calculating hopkins stats to detect if the data set has clusters..."
+	X = extract_data("train.data.file", "train.data.feature.fields")	
+	X_ran = extract_data("expl.data.file", "expl.data.feature.fields")
+	split_size = len(X_ran)
+	
+	(X_spl, X_tra) = split_data_random(X, split_size)
+	min_dist_ran = find_min_distances(X_ran, X_tra)
+	min_dist_spl = find_min_distances(X_spl, X_tra)
+	
+	print "random"
+	print min_dist_ran
+	print "sum"
+	ran_sum = min_dist_ran.sum()
+	print ran_sum
+	
+	
+	print "split"
+	print min_dist_spl
+	print "sum"
+	spl_sum = min_dist_spl.sum()
+	print spl_sum
+	
+	hopkins_stat = spl_sum / (ran_sum + spl_sum)
+	print "hopkins stats %.3f" %(hopkins_stat)
+	
+	
+	
+	
+	
+	
+			
