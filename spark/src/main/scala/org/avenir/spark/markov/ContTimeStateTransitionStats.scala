ce7935d970d9938864a6b657f17f3a1219567b48
Pranab Ghosh <pkghosh99@gmail.com>
2016-09-29 22:34:28 -0700

    expected dwell time in state

--- spark/src/main/scala/org/avenir/spark/markov/ContTimeStateTransitionStats.scala
+++ spark/src/main/scala/org/avenir/spark/markov/ContTimeStateTransitionStats.scala
@@ -27,6 +27,7 @@ import org.chombo.util.DoubleTable
 import java.text.SimpleDateFormat
 import org.chombo.util.Utility
 import org.chombo.util.Matrix
+import org.chombo.util.PoisonDistr
 
  /**
   * Calculates statistics for continuous time markov chain process
@@ -54,7 +55,17 @@ import org.chombo.util.Matrix
 	   val numStates = states.size()
 	   val timeHorizon = appConfig.getInt("time.horizon")
 	   val stateTransFilePath = appConfig.getString("state.trans.file.path")
-	   
+	   val transStats = appConfig.getString("state.trans.stats")
+	   val targetState = appConfig.getString("target.state")
+	   val targetStateIndx = states.indexOf(targetState)
+	   val endStateDefined = appConfig.hasPath("end.state")
+	   val endStateIndx = endStateDefined match  {
+	     case true => states.indexOf(appConfig.getString("end.state"))
+	     case false => -1
+	   }
+	   val debugOn = appConfig.getBoolean("debug.on")
+	   val saveOutput = appConfig.getBoolean("save.output")
+	   	   
 	   //state transition rates
 	   val stateTransData = sparkCntxt.textFile(stateTransFilePath)
 	   val stateTrans = stateTransData.map(line => {
@@ -68,7 +79,8 @@ import org.chombo.util.Matrix
   
 	   //normalized state transition rate and multiplications
 	   val idenMatrx = new Matrix()
-	   idenMatrx.initialize(numStates, numStates,1.0)
+	   idenMatrx.initialize(numStates, numStates,0).initializeDiagonal(1)
+	   
 	   val stateTransMult  = stateTrans.map(keySt => {
 	     val trans = keySt._2
 	     val maxRate = -trans.getMinDiagonalElement()
@@ -77,23 +89,67 @@ import org.chombo.util.Matrix
 	     val count = maxRate * timeHorizon
 	     val limit = (4 + 6 * Math.sqrt(count) + count).toInt
 	     
-	     var matrixPowers = List[Matrix](idenMatrx)
-	     matrixPowers = discreetTrans :: matrixPowers
+	     val matrixPowers = new Array[Matrix](limit+1)
+	     matrixPowers(0) = idenMatrx
+	     matrixPowers(1) = discreetTrans
 	     for (i <- 2 to limit) {
-	       val next = matrixPowers.head.dot(discreetTrans)
-	       matrixPowers = next :: matrixPowers
+	       val next = matrixPowers.last.dot(discreetTrans)
+	       matrixPowers(i) = next
 	     }
 	     
-	     (keySt._1, matrixPowers)
+	     (keySt._1, maxRate, matrixPowers)
 	   })
 	   
 	   //collect and convert to map
-	   val stateTransMultMap = scala.collection.mutable.Map[Record, List[Matrix]]()
+	   val stateTransMultMap = scala.collection.mutable.Map[Record, (Double, Array[Matrix])]()
 	   stateTransMult.collect.foreach(s => {
-	     stateTransMultMap += (s._1 -> s._2.reverse)
+	     stateTransMultMap += (s._1 -> (s._2,s._3))
 	   })
 	   
+	   //initial state
+	   val brStateTransMultMap = sparkCntxt.broadcast(stateTransMultMap)
+	   val initState = sparkCntxt.textFile(inputPath)
 	   
+	   //statistic
+	   val stat = transStats match {
+	     case "dwellTime" => {
+		   initState.map(line => {
+		     val items = line.split(fieldDelimIn)
+		     val key = Record(items, 0, keyFieldLen)
+		     val initState = items(keyFieldLen)
+		     val initStateIndx = states.indexOf(initState)
+		     val (maxRate:Double, transMult:Array[Matrix]) = brStateTransMultMap.value.get(key).get
+		     val count = maxRate * timeHorizon
+		     val limit = (4 + 6 * Math.sqrt(count) + count).toInt
+		
+		     val poison = new PoisonDistr(count)
+		     
+		     var sumOuter:Double = 0
+		     for (i <- 0 to limit) {
+		       var sumInner:Double = 0
+		       for (j <- 0 to i) {
+		         val startTargetPr = transMult(j).get(initStateIndx, targetStateIndx)
+		         val targetEndPr = endStateDefined match {
+		           case true => transMult(i-j).get(targetStateIndx, endStateIndx)
+		           case false => 1.0
+		         }
+		         sumInner += (startTargetPr * targetEndPr)
+		       }
+		       sumOuter += (timeHorizon/(i + 1)) * sumInner * poison.next()
+		     }
+		     (key,sumOuter)
+		   })
+
+	     }
+	     
+	     case _ => {
+	    	 throw new IllegalArgumentException("invalid state transition stats")
+	     }
+	   }
+	   
+	   if(saveOutput) {	   
+	   	stat.saveAsTextFile(outputPath) 
+   	   }
 	   
    }
 }
