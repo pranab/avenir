d1bc594d62de511c412eb336ae87909a4b4f0218
Pranab Ghosh <pkghosh99@gmail.com>
2016-11-12 10:16:14 -0800

    output formatting

--- resource/carm.sh
+++ resource/carm.sh
@@ -17,17 +17,31 @@ case "$1" in
 "mutInfo")  
 	echo "running MR  MutualInformation"
 	CLASS_NAME=org.avenir.explore.MutualInformation
-	IN_PATH=$HDFS_BASE_DIR/input
-	OUT_PATH=$HDFS_BASE_DIR/output
+	IN_PATH=$HDFS_BASE_DIR/mut/input
+	OUT_PATH=$HDFS_BASE_DIR/mut/output
 	echo "input $IN_PATH output $OUT_PATH"
 	hdfs dfs -rm -r $OUT_PATH
 	echo "removed output dir $OUT_PATH"
 	yarn jar $JAR_NAME $CLASS_NAME -Dconf.path=$PROP_FILE $IN_PATH $OUT_PATH
-	hdfs dfs -rm -r $HDFS_BASE_DIR/output/_logs
-	hdfs dfs -rm -r $HDFS_BASE_DIR/output/_SUCCESS	
-	hdfs dfs -ls $HDFS_BASE_DIR/output
+	hdfs dfs -rm -r $HDFS_BASE_DIR/mut/output/_logs
+	hdfs dfs -rm -r $HDFS_BASE_DIR/mut/output/_SUCCESS	
+	hdfs dfs -ls $HDFS_BASE_DIR/mut/output
     ;;
 
+
+"classAffinity")  
+	echo "running MR  CategoricalClassAffinity"
+	CLASS_NAME=org.avenir.explore.CategoricalClassAffinity
+	IN_PATH=$HDFS_BASE_DIR/cca/input
+	OUT_PATH=$HDFS_BASE_DIR/cca/output
+	echo "input $IN_PATH output $OUT_PATH"
+	hdfs dfs -rm -r $OUT_PATH
+	echo "removed output dir $OUT_PATH"
+	yarn jar $JAR_NAME $CLASS_NAME -Dconf.path=$PROP_FILE $IN_PATH $OUT_PATH
+	hdfs dfs -rm -r $HDFS_BASE_DIR/cca/output/_logs
+	hdfs dfs -rm -r $HDFS_BASE_DIR/cca/output/_SUCCESS	
+	hdfs dfs -ls $HDFS_BASE_DIR/cca/output
+    ;;
 
 *) 
 	echo "unknown operation $1"
