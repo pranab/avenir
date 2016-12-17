01e1fcc76bf4f57a2a0dd610c0fa443d1d42c331
Pranab Ghosh <pkghosh99@gmail.com>
2016-12-17 05:24:54 -0800

    similarity between grouped records

--- spark/src/main/scala/org/avenir/spark/similarity/GroupedRecordSimilarity.scala
+++ spark/src/main/scala/org/avenir/spark/similarity/GroupedRecordSimilarity.scala
@@ -26,12 +26,8 @@ import org.chombo.util.BasicUtils
 import org.chombo.distance.InterRecordDistance
 import scala.collection.mutable.ListBuffer
 
+object GroupedRecordSimilarity extends JobConfiguration {
 
-/**
- * @param args
- * @return
- */
-object RecordSimilarity extends JobConfiguration {
    /**
     * @param args
     * @return
@@ -53,67 +49,48 @@ object RecordSimilarity extends JobConfiguration {
 	   val distAttrSchemaPath = getMandatoryStringParam(appConfig, "dist.attr.schema.path")
 	   val distAttrSchema = BasicUtils.getDistanceSchema(distAttrSchemaPath)
 	   val distFinder = new InterRecordDistance(richAttrSchema, distAttrSchema, fieldDelimIn)
-	   val numBuckets = getIntParamOrElse(appConfig, "num.buckets", 16)
-	   val buckets  = List.range(0, numBuckets)
+	   val groupFieldOrdinals = getMandatoryIntListParam(appConfig, "group.field.ordinals").asScala
 	   val debugOn = getBooleanParamOrElse(appConfig, "debug.on", false)
 	   val saveOutput = getBooleanParamOrElse(appConfig, "save.output", true)
 	   
 	   //read input
 	   val data = sparkCntxt.textFile(inputPath)
-
+	   
 	   //key with all bucket pairs and record as value
-	   val bucketedData = data.flatMap(line => {
+	   val groupedData = data.map(line => {
 		   val items = line.split(fieldDelimIn, -1)
-		   val keyRec = Record(items, keyFieldOrdinals)
-		   val hash = keyRec.hashCode
-		   val thisBucket = (if (hash < 0) -hash else hash) % numBuckets
-		   var bucketId = 0
-		   val bucketedRec = buckets.map(b => {
-		     val bucketPairHash = if (thisBucket > b) {
-		        bucketId = 0
-		        thisBucket << 12 | b
-		     } else { 
-		        bucketId = 1
-		        b << 12 | thisBucket  
-		     }
-		       
-		     (bucketPairHash, (bucketId,line)) 
-		   })
-		   
-		   bucketedRec
+		   val groupRec = Record(items, groupFieldOrdinals)
+		   (groupRec, line)
 	   })
 	   
 	   //group by key and generate distances
-	   val bucketedDistances = bucketedData.groupByKey().flatMapValues(recs => {
-	     val firstBucket = recs.filter(r => r._1 == 0)
-	     val secondBucket = recs.filter(r => r._1 == 1)
+	   val groupedDistances = groupedData.groupByKey().flatMapValues(recs => {
+	     val size = recs.size
+	     val records = recs.toArray
 	     var distances = new ListBuffer[(Record, Record, Double)]()
-	     firstBucket.foreach(f => {
-	       val firstRec = f._2
-	       val firstKey = Record(firstRec.split(fieldDelimIn, -1), keyFieldOrdinals)
-	       secondBucket.foreach(s => {
-	    	   val secondRec = s._2
-	    	   val secondKey = Record(secondRec.split(fieldDelimIn, -1), keyFieldOrdinals)
-	    	   val dist = distFinder.findDistance(firstRec, secondRec)
+
+	     for (i <- 0 to (size -1)) {
+	       val firstKey = Record(records(i).split(fieldDelimIn, -1), keyFieldOrdinals)
+	       for (j <- (i+1) to (size-1)) {
+	    	   val secondKey = Record(records(j).split(fieldDelimIn, -1), keyFieldOrdinals)
+	    	   val dist = distFinder.findDistance(records(i), records(j))
 	    	   distances += ((firstKey, secondKey, dist))
-	       })
-	     })
+	       }
+	     }
 	     distances
 	   })
 	   
-	   //only distances
-	   val distances = bucketedDistances.values
-	   
 	   if (debugOn) {
-	     val distCol = distances.collect
+	     val distCol = groupedDistances.collect
 	     distCol.foreach(d => {
 	       println(d)
 	     })
 	   }	
 	   
 	   if (saveOutput) {
-	     distances.saveAsTextFile(outputPath)
+	     groupedDistances.saveAsTextFile(outputPath)
 	   }
 	   
-   }
+	   
+   }  
 }
