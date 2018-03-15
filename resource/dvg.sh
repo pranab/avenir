e56bd07d8426af8231b836aba207b99abf555926
Pranab Ghosh <pkghosh99@gmail.com>
2018-03-15 16:36:15 -0700

    categorical to dummy binary transformation

---
+++ resource/dvg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#converts high cardinality categorical aatributes to numerical
+
+PROJECT_HOME=/Users/pranab/Projects/bin/avenir
+JAR_NAME=$PROJECT_HOME/uber-avenir-spark-1.0.jar
+MASTER=spark://akash.local:7077
+
+case "$1" in
+
+"binValGen")	
+	echo "running MultiArmBandit"
+	CLASS_NAME=org.avenir.spark.util.BinaryDummyVariableGenerator
+	INPUT=file:///Users/pranab/Projects/bin/avenir/input/dvg/leads.txt
+	OUTPUT=file:///Users/pranab/Projects/bin/avenir/output/dvg
+	rm -rf ./output/dvg
+	$SPARK_HOME/bin/spark-submit --class $CLASS_NAME   \
+	--conf spark.ui.killEnabled=true --master $MASTER $JAR_NAME  $INPUT $OUTPUT dvg.conf
+	ls -l ./output/samp
+	;;
+
+*) 
+	echo "unknown operation $1"
+	;;
+
+esac
