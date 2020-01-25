5c60bf6b44337da7a2e32d3762b90e7c57d4aa08
Pranab Ghosh <pkghosh99@gmail.com>
2020-01-25 03:25:52 -0800

    time series generation with trend, cycle

--- python/app/tsexp.py
+++ python/app/tsexp.py
@@ -23,6 +23,7 @@ from datetime import datetime
 from statsmodels.graphics import tsaplots
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.stattools import kpss
+from statsmodels.stats.stattools import jarque_bera
 from matplotlib import pyplot
 sys.path.append(os.path.abspath("../lib"))
 from util import *
@@ -37,9 +38,17 @@ def loadConfig(configFile):
 	defValues["data.filePath"] = (None, "missing file path")
 	defValues["data.col.index"] = (None, "missing col index")
 	defValues["data.row.range"] = ("all", None)
+	defValues["diff.order"] = (1, None)
 	defValues["acf.lags"] = (40, None)
 	defValues["acf.alpha"] = (None, None)
 	defValues["acf.diff"] = (False, None)
+	defValues["pacf.lags"] = (40, None)
+	defValues["pacf.alpha"] = (None, None)
+	defValues["adf.regression"] = ("c", None)
+	defValues["adf.autolag"] = ("AIC", None)
+	defValues["kpss.regression"] = ("c", None)
+	defValues["hist.cumulative"] = (False, None)
+	defValues["hist.density"] = (False, None)
 	
 	config = Configuration(configFile, defValues)
 	return config
@@ -63,10 +72,20 @@ if __name__ == "__main__":
 	config = loadConfig(confFile)
 	data = loadData(config)
 	
+	#plot data
 	if op == "draw":
 		pyplot.plot(data)
 		pyplot.show()
 	
+	#difference
+	elif op == "diff":
+		order = config.getIntConfig("diff.order")[0]
+		for i in range(order):
+			data = difference(data)
+		pyplot.plot(data)
+		pyplot.show()
+
+	#auto correlation
 	elif op == "acf":
 		#print(data)
 		diff = config.getBooleanConfig("acf.diff")[0]
@@ -77,28 +96,54 @@ if __name__ == "__main__":
 			
 		lags = config.getIntConfig("acf.lags")[0]
 		alpha =config.getFloatConfig("acf.alpha")[0]
-		tsaplots.plot_acf(np.array(data), lags = lags, alpha = alpha)
+		tsaplots.plot_acf(data, lags = lags, alpha = alpha)
+		pyplot.show()
+
+	#partial auto correlation
+	elif op == "pacf":
+		lags = config.getIntConfig("pacf.lags")[0]
+		alpha =config.getFloatConfig("pacf.alpha")[0]
+		tsaplots.plot_pacf(data, lags = lags, alpha = alpha)
 		pyplot.show()
 	
+	#stationarity test
 	elif op == "adf":
-		result = adfuller(data, autolag='AIC')
+		regression = config.getStringConfig("adf.regression")[0]
+		autolag = config.getStringConfig("adf.autolag")[0]
+		result = adfuller(data, regression=regression, autolag=autolag)
 		print(f'ADF Statistic: {result[0]}')
 		print(f'p value: {result[1]}')
 		print(f'num lags: {result[2]}')
 		print(f'num observation for regression: {result[3]}')
+		print('Critial Values:')
 		for key, value in result[4].items():
-			print('Critial Values:')
 			print(f'   {key}, {value}')    
 			
+	#stationarity test
 	elif op =="kpss":
-		statistic, pvalue, nLags, criticalValues = kpss(data)
+		regression = config.getStringConfig("kpss.regression")[0]
+		statistic, pvalue, nLags, criticalValues = kpss(data, regression=regression)
 		print(f'KPSS Statistic: {statistic}')
 		print(f'pvalue: {pvalue}')
 		print(f'num lags: {nLags}')
 		print('Critial Values:')
 		for key, value in criticalValues.items():
-			print(f'   {key} : {value}')		
+			print(f'   {key} : {value}')	
+				
+	#normalcy test	
+	elif op == "jarqBera":	
+		jb, jbpv, skew, kurtosis =  jarque_bera(data)
+		print(f'pvalue: {jbpv}')
+		print(f'skew: {skew}')
+		print(f'kurtosis: {kurtosis}')
 		
+	#histogram
+	elif op == "hist":	
+		cumulative = config.getBooleanConfig("hist.cumulative")[0]
+		density = config.getBooleanConfig("hist.density")[0]
+		pyplot.hist(data, cumulative=cumulative, density=density)
+		pyplot.show()
+
 		
 	else:
 		raise ValueError("unknown command")
---
+++ python/app/tsgen.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+
+# avenir-python: Machine Learning
+# Author: Pranab Ghosh
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0 
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import os
+import sys
+from random import randint
+import time
+from datetime import datetime
+sys.path.append(os.path.abspath("../lib"))
+from util import *
+from mlutil import *
+from sampler import *
+
+"""
+Time series generation with optional trend, cycles and random remainder components
+"""
+
+def loadConfig(configFile):
+	"""
+	load config file
+	"""
+	defValues = {}
+	defValues["window.size"] = (None, "missing time window size")
+	defValues["window.samp.interval"] = ("fixed", None)
+	defValues["window.samp.interval.params"] = (None, "missing time interval parameters")
+	defValues["window.samp.align.unit"] = (None, None)
+	defValues["ts.base"] = ("mean", None)
+	defValues["ts.base.params"] = (None, "missing time series base parameters")
+	defValues["ts.trend"] = ("nothing", None)
+	defValues["ts.trend.params"] = (None, None)
+	defValues["ts.cycles"] = ("nothing", None)
+	defValues["ts.cycle.year.params"] = (None, None)
+	defValues["ts.cycle.week.params"] = (None, None)
+	defValues["ts.cycle.day.params"] = (None, None)
+	defValues["ts.random"] = (True, None)
+	defValues["ts.random.params"] = (None, None)
+	defValues["ts.value.type"] = ("float", None)
+	defValues["ts.value.precision"] = (3, None)
+	defValues["ts.time.format"] = ("epoch", None)
+	
+
+	config = Configuration(configFile, defValues)
+	return config
+
+
+if __name__ == "__main__":
+	confFile = sys.argv[1]
+	config = loadConfig(confFile)
+	delim = ","
+	
+	winSz = config.getStringConfig("window.size")[0]
+	items = winSz.split("_")
+	curTm, pastTm = pastTime(int(items[0]), items[1])
+	
+	sampIntvType = config.getStringConfig("window.samp.interval")[0]
+	sampIntv = config.getStringConfig("window.samp.interval.params")[0].split(delim)
+	intvDistr = None
+	if sampIntvType == "fixed":
+		sampIntv = int(sampIntv[0])
+	elif sampIntvType == "random":
+		siMean = float(sampIntv[0])
+		siSd = float(sampIntv[1])
+		intvDistr = GaussianRejectSampler(siMean,siSd)
+	else:
+		raise ValueError("invalid sampling interval type")
+		
+	sampAlignUnit = config.getStringConfig("window.samp.align.unit")[0]
+	
+	tsBaseType = config.getStringConfig("ts.base")[0]
+	items = config.getStringConfig("ts.base.params")[0].split(delim)
+	if tsBaseType == "mean":
+		tsMean = float(items[0])
+	elif tsBaseType == "exp":
+		tsAlpha = float(items[0])
+		tsHist = toFloatList(items[1:])
+	else:
+		raise ValueError("invalid base type")
+		
+	tsTrendType = config.getStringConfig("ts.trend")[0]
+	items = config.getStringConfig("ts.trend.params")[0].split(delim)
+	if tsTrendType == "linear":
+		tsTrendSlope = float(items[0])
+	elif tsTrendType == "quadratic":
+		tsTrendQuadParams = toFloatList(items)
+	else:
+		raise ValueError("invalid trend type")
+		
+	cycles = config.getStringConfig("ts.cycles")[0].split(delim)
+	yearCycle = weekCycle = dayCycle = None
+	for c in cycles:
+		key = "ts.cycle." + c + ".params"
+		cycleValues = config.getStringConfig(key)[0].split(delim)
+		if c == "year":
+			yearCycle = toFloatList(cycleValues)
+		elif c == "week":
+			weekCycle = toFloatList(cycleValues)
+		elif c == "day":
+			dayCycle = toFloatList(cycleValues)
+			
+		
+	tsRandom = config.getBooleanConfig("ts.random")[0]
+	if tsRandom:
+		items = config.getStringConfig("ts.random.params")[0].split(delim)
+		tsRandMean = float(items[0])
+		tsRandStdDev = float(items[1])
+		tsRandDistr = GaussianRejectSampler(tsRandMean,tsRandStdDev)
+		
+	tsValType = config.getStringConfig("ts.value.type")[0]
+	valPrecision = config.getIntConfig("ts.value.precision")[0]
+	tsTimeFormat = config.getStringConfig("ts.time.format")[0]
+	
+	if sampAlignUnit:
+		pastTm = timeAlign(pastTm, sampAlignUnit)	
+	
+	if intvDistr:
+		sampIntv = int(intvDistr.sample())
+	
+	sampTm = pastTm
+	counter = 0
+	if tsValType == "int":
+		ouForm = "{},{}"
+	else:
+		ouForm = "{},{:."  + str(valPrecision) + "f}"
+
+	while (sampTm < curTm):
+		curVal = 0
+		
+		#base
+		if tsBaseType == "mean":
+			curVal = tsMean
+		else:
+			alphaInv = 1
+			for i in reversed(range(lenn(tsHist))):
+				curVal = tsAplha * alphaInv * tsHist[i] 
+				alphaInv *= (1.0 - tsAplha)
+		
+		#trend
+		if tsTrendType == "linear":
+			curVal += counter * tsTrendSlope
+		elif tsTrendType == "quadratic":
+			curVal = tsTrendQuadParams[0] * counter + tsTrendQuadParams[1] * counter * counter
+		counter += 1
+		
+		#cycle
+		if yearCycle:
+			month = monthOfYear(sampTm)
+			curVal += yearCycle[month]
+		if weekCycle:
+			day = dayOfWeek(sampTm)
+			curVal += weekCycle[day]
+		if dayCycle:
+			hour = hourOfDay(sampTm)
+			curVal += dayCycle[hour]
+
+		#random remainder
+		if tsRandStdDev:
+			curVal += tsRandDistr.sample()
+	
+		#date time
+		if tsTimeFormat == "epoch":
+			dt = sampTm
+		else:
+			dt = datetime.fromtimestamp(sampTm)
+			dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+		
+		#update history
+		if tsBaseType == "exp":
+			tsHist.append(curVal)
+			tsHist.pop(0)
+		
+		#value
+		if tsValType == "int":
+			curVal = int(curVal)
+
+		print(ouForm.format(dt, curVal))
+	
+		if intvDistr:
+			sampIntv = int(intvDistr.sample())
+		sampTm += sampIntv
+		
+	
+
