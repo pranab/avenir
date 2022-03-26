e12323024adc9327ec211e7e65c8e9a4935efc0c
Pranab Ghosh <pkghosh99@gmail.com>
2022-03-25 21:20:04 -0700

    multi arm bandit

---
+++ python/reinf/bandit.py
@@ -0,0 +1,70 @@
+#!/usr/local/bin/python3
+
+# avenir-python: Machine Learning
+# Author: Pranab Ghosh
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0 
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import os
+import sys
+import random 
+import math
+import numpy as np
+sys.path.append(os.path.abspath("../lib"))
+from util import *
+from mlutil import *
+
+class Action(object):
+	"""
+	action class for  multi arm bandit 
+	"""
+	
+	def __init__(self, name, wsize):
+		"""
+		initializer
+		
+		Parameters
+			naction : no of actions
+			rwindow : reward window size
+		"""
+		self.name = name
+		self.available = True
+		self.rwindow = RollingStat(wsize)
+		self.nplay = 0
+
+	def makeAvailable(self, available):
+		"""
+		sets available flag
+		
+		Parameters
+			available : available flag
+		"""
+		self.available = available
+	
+	def addReward(self, reward):
+		"""
+		add reward
+		
+		Parameters
+			reward : reward value
+		"""
+		self.rwindow.add(reward)	
+		
+	def getAverageReward(self):
+		"""
+		get average reward
+		"""
+		rav, rsd = self.rwindow.getStat()
+		return rav
+	
+	
---
+++ python/reinf/ucb.py
@@ -0,0 +1,106 @@
+#!/usr/local/bin/python3
+
+# avenir-python: Machine Learning
+# Author: Pranab Ghosh
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0 
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import os
+import sys
+import random 
+import math
+import numpy as np
+sys.path.append(os.path.abspath("../lib"))
+from util import *
+from mlutil import *
+from sampler import *
+
+class UpperConfBound:
+	"""
+	upper conf bound multi arm bandit (ucb1)
+	"""
+	
+	def __init__(self, actions, wsize, transientAction=False):
+		"""
+		initializer
+		
+		Parameters
+			naction : no of actions
+			rwindow : reward window size
+			transientAction ; if decision involves some tied up resource it should be set True
+		"""
+		assertGreater(wsize, 9, "window size should be at least 10")
+		self.actions = list(map(lambda aname : RollingStat(aname, wsize), actions))
+		self .totPlays = 0
+		self.transientAction = transientAction
+		
+	def act(self):
+		"""
+		next play return selected action
+		"""
+		sact = None
+		scmax = 0
+		for act in self.actions:
+			if act.nplay == 0:
+				sact = act
+				break
+			else:		
+				if self.transientAction or act.available:
+					sc  = act.getAverageReward() * sqrt(2 * math.log(self .totPlays) / act.nplay)
+					if sc > scmax:
+						scmax = sc
+						sact = act
+			
+			if not self.transientAction:
+				sact.makeAvailable(False)
+			sact.nplay += 1
+			self .totPlays += 1
+			
+		return sact.name
+			
+	def setReward(self, aname, reward):
+		"""
+		reward feedback for action
+			
+		Parameters
+			act : action
+			reward : reward value
+		"""
+		acts = list(filter(lambda act : act.name == aname, self.actions))
+		assertEqual(len(acts), 1, "invalid action name")
+		acts[0].addReward(reward)
+		if not self.transientAction:
+			sact.makeAvailable(True)
+		
+	@staticmethod
+	def save(model, filePath):
+		"""
+		saves object
+				
+		Parameters
+			model : model object
+			filePath : file path
+		"""
+		saveObject(model, filePath)
+			
+	@staticmethod
+	def restore(filePath):
+		"""
+		restores object
+				
+		Parameters
+			filePath : file path
+		"""
+		model = restoreObject(filePath)
+		return model
+	
